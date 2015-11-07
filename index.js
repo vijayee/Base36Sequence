5256256a83325993b28b5f1ef1603c344beb2bed
Vijayee Kulkaa <vijayee.kulkaa@hushmail.com>
2015-11-07 13:51:30 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+    This module starts a sequence from the given base10 or base36 encoded integer
+*/
+var sequence= function(start){
+    var current;
+
+    if (!Number.isInteger(start)){
+        current= this.decode(start) || 0;
+    }
+    else {
+        current = start;
+    }
+    //Display current Sequence Number
+    this.current= function(){
+        return current.toString(36);
+    };
+    //Get next Sequence Number and increment sequence
+    this.next= function(){
+        var next= this.current.toString(36);
+        current++;
+        return next
+    };
+};
+//Decode a base36 integer
+sequence.prototype.decode= function(num){
+    if (!this.isBase36(num)){
+        return null;
+    }
+    return parseInt(num, 36);
+};
+//Encode an integer into a base36 integer
+sequence.prototype.encode= function(num){
+    if (!Number.isInteger(num)){
+        return 0;
+    }
+    return num.toString(36);
+};
+//Detect whether a string is a base36 integer
+sequence.prototype.isBase36= function(num){
+    var re= /^(0x|0X)?[a-zA-Z0-9]+$/;
+    return re.test(num);
+}
+
+module.exports= sequence;
